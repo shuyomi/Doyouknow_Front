5d93486e3d98cd4b091d0f8681331ae51fa27ad5
dongyeop00 <ehd_duq94@naver.com>
2025-03-22 20:11:04 +0900

    Feat : FAQ 페이지 구현

---
+++ src/components/Faq/FaqNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/Faq/FaqNav.css";
+
+const FaqNav = () => {
+    return (
+        <div className="faq-nav">
+            <h1>FAQ</h1>
+            <hr className="faq-hr" />
+            <h2>자주 묻는 질문</h2>
+        </div>
+    );
+};
+
+export default FaqNav;
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,7 +1,16 @@
 import React from "react";
+import FaqHeader from "../components/Faq/FaqHeader";
+import FaqNav from "../components/Faq/FaqNav";
+import FaqList from "../components/Faq/FaqList";
 
 const Faq = () => {
-    return <></>;
+    return (
+        <div>
+            <FaqHeader />
+            <FaqNav />
+            <FaqList />
+        </div>
+    );
 };
 
 export default Faq;
