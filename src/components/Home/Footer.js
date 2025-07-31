2fd4681ca42803cf863eda1db90a2de451832454
dongyeop00 <ehd_duq94@naver.com>
2025-07-31 21:20:34 +0900

    Fix : Faq 푸터 추가

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -4,45 +4,20 @@ import "../../styles/Home/Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-        <a
-            href="https://portal.anyang.ac.kr/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-item"
-        >
-            <i
-        className="pi pi-external-link footer-icon"
-        style={{ margin: 'auto', paddingBottom:'5px'}}
-        ></i>
-            <span>학교 포털</span>
-        </a>
-        <a
-            href="https://www.anyang.ac.kr/main/index.do"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-item"
-        >
-             <i
-        className="pi pi-globe footer-icon"
-        style={{  margin: 'auto', paddingBottom:'5px' }}
-        ></i>
-            <span>공식 사이트</span>
-        </a>
+            <a href="https://portal.anyang.ac.kr/" target="_blank" rel="noopener noreferrer" className="footer-item">
+                <i className="pi pi-external-link footer-icon" style={{ margin: "auto", paddingBottom: "5px" }}></i>
+                <span>학교 포털</span>
+            </a>
+            <a href="https://www.anyang.ac.kr/main/index.do" target="_blank" rel="noopener noreferrer" className="footer-item">
+                <i className="pi pi-globe footer-icon" style={{ margin: "auto", paddingBottom: "5px" }}></i>
+                <span>공식 사이트</span>
+            </a>
 
-        <a
-         href="https://foam-surfboard-927.notion.site/20c06665d9df800aa1a1e32c5fb8cf75?pvs=105"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-item"
-        >
-        <i
-        className="pi pi-comments footer-icon"
-        style={{ margin: 'auto', paddingBottom:'5px' }}
-        ></i>
-        <span>문의하기</span>
-        </a>
+            <a href="https://foam-surfboard-927.notion.site/20c06665d9df800aa1a1e32c5fb8cf75?pvs=105" target="_blank" rel="noopener noreferrer" className="footer-item">
+                <i className="pi pi-comments footer-icon" style={{ margin: "auto", paddingBottom: "5px" }}></i>
+                <span>문의하기</span>
+            </a>
         </div>
-
     );
 };
 
